7eabe61859d82ec0c75b93a44bda55ca37b894d3
Vitor Marcato <vitor.marcato@alura.com.br>
2024-06-06 19:04:01 -0300

    chore: Initial commit

---
+++ src/test/java/br/com/study/cdc/CdcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.study.cdc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
